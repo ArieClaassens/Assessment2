fb43bd5d39c39c88b7ab027918ef0980573e9cf8
Arie <arieclaassens@gmail.com>
2014-12-06 11:12:36 +0200

    Update for Coverity scan automation via Travis CI

--- JavaGIS/src/javagis/JavaGIS.java
+++ JavaGIS/src/javagis/JavaGIS.java
@@ -31,12 +31,18 @@ package javagis;
  */
 public class JavaGIS {
 
+    
+    public JavaGIS() {
+        System.out.println("You started it.....");
+    }
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
         new JavaGIS();
+        
     }
 
 }
