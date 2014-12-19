e8e4cef578471fe36cb022853e5e0940f1c1e667
Arie <arieclaassens@gmail.com>
2014-12-19 18:30:24 +0200

    Pulling in Help file contents from help.html now Signed-off-by:Arie <arieclaassens@gmail.com>

--- JavaGIS/src/BacterialBomb/HTMLHelp.java
+++ JavaGIS/src/BacterialBomb/HTMLHelp.java
@@ -2,13 +2,18 @@ package BacterialBomb;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 import javax.swing.JEditorPane;
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 import javax.swing.SwingUtilities;
 import javax.swing.text.Document;
 import javax.swing.text.html.HTMLEditorKit;
-import javax.swing.text.html.StyleSheet;
 
 /**
  * A complete Java class that demonstrates how to create an HTML viewer with styles, using the JEditorPane,
@@ -42,20 +47,39 @@ public class HTMLHelp {
                 jEditorPane.setEditorKit(kit);
 
                 // add some styles to the html
-                StyleSheet styleSheet = kit.getStyleSheet();
-                styleSheet.addRule("body {color:#000; font-family:times; margin: 4px; }");
-                styleSheet.addRule("h1 {color: blue;}");
-                styleSheet.addRule("h2 {color: #ff0000;}");
-                styleSheet.addRule("pre {font : 10px monaco; color : black; background-color : #fafafa; }");
+                //StyleSheet styleSheet = kit.getStyleSheet();
+                //styleSheet.addRule("body {color:#000; font-family:times; margin: 4px; }");
+                //styleSheet.addRule("h1 {color: blue;}");
+                //styleSheet.addRule("h2 {color: #ff0000;}");
+                //styleSheet.addRule("pre {font : 10px monaco; color : black; background-color : #fafafa; }");
+                
+                //From http://www.avajava.com/tutorials/lessons/how-do-i-convert-a-web-page-to-a-string.html and
+                //http://www.java2s.com/Code/JavaAPI/java.lang/ClassgetResourceStringnamerelativetotheclasslocation.htm
+                String htmlString = "";
+                
+                try {
+			URL url = DispersalModeller.class.getResource("/bacterialbomb/help.html");
+			URLConnection urlConnection = url.openConnection();
+			InputStream is = urlConnection.getInputStream();
+			InputStreamReader isr = new InputStreamReader(is);
 
-                // create some simple html as a string
-                String htmlString = "<html>\n"
-                        + "<body>\n"
-                        + "<h1>BADdm Help</h1>\n"
-                        + "<h2>This is an H2 header</h2>\n"
-                        + "<p>This is some sample text</p>\n"
-                        + "<p><a href=\"http://www.geog.leeds.ac.uk/\">www.geog.leeds.ac.uk</a></p>\n"
-                        + "</body>\n";
+			int numCharsRead;
+			char[] charArray = new char[1024];
+			StringBuffer sb = new StringBuffer();
+			while ((numCharsRead = isr.read(charArray)) > 0) {
+				sb.append(charArray, 0, numCharsRead);
+			}
+			String result = sb.toString();
+                        htmlString = result;
+
+			System.out.println("*** BEGIN ***");
+			System.out.println(result);
+			System.out.println("*** END ***");
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
                 // create a document, set it on the jeditorpane, then add the html
                 Document doc = kit.createDefaultDocument();
@@ -69,11 +93,10 @@ public class HTMLHelp {
                 // make it easy to close the application
                 //Disable this action to prevent the Kit from killing the whole application NB!!!!!!
                 //j.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
                 // display the frame
-                j.setSize(new Dimension(400, 400));
+                j.setSize(new Dimension(600, 400));
 
-        // pack it, if you prefer
+                // pack it, if you prefer
                 //j.pack();
                 // center the jframe, then make it visible
                 j.setLocationRelativeTo(null);
